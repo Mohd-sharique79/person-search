5162d03b0c0a8a2f3cb2e92d964b534d57684f71
Mohd Sharique <mohdsharik8543@gmail.com>
2025-02-03 21:48:31 +0530

    Enabled Edit

--- app/components/user-card.tsx
+++ app/components/user-card.tsx
@@ -6,6 +6,7 @@ import { Badge } from '@/components/ui/badge'
 import { Phone, Mail } from 'lucide-react'
 import { getUserById } from '../actions/actions'
 import DeleteButton from './delete-button'
+import { UserEditDialog } from './user-edit-dialog'
 
 interface UserCardProps {
   userId: string
@@ -46,7 +47,8 @@ export default async function UserCard({ userId }: UserCardProps) {
         )}
       </CardContent>
       <CardFooter className="flex justify-between items-center">
-        <DeleteButton userId={user.id} />
+        <DeleteButton userId={user.id} /> 
+        <UserEditDialog user={user}/>
       </CardFooter>
     </Card>
   )
